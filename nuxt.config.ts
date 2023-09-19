f8620d34f8861e9a82460e4d77c115b5005b1e41
Howell Martinez <howellmartinez@gmail.com>
2023-09-19 16:19:08 +0800

    modify nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: [ '@nuxt/content', '@nuxtjs/tailwindcss']
+  modules: [ '@nuxt/content', '@nuxtjs/tailwindcss'],
+  routeRules: {
+    // all routes will be background revalidated (ISR) at most every 60 seconds
+    '/**': { isr: 60 },
+    // this page will be generated on demand and cached permanently
+    '/static': { isr: true }
+  },
 })
